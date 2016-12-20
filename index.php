a2b142a6930c83ab0adfc8a1f8f1c696c03bc850
m-kondratenko <madcat1202@yandex.ru>
2016-12-20 14:29:54 +0300

    index.php with main blocks has been added

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>URL Shortener</title>
+    <link rel="stylesheet" href="css/main.css" title="test umbrella">
+    <script src="js/jquery.min.js"></script>
+  </head>
+  <body>
+    <h1>URL Shortener</h1>
+    <form method="post">
+      <p>Insert your URL:</p>
+      <input id="longurl" type="text"><br>
+      <p>Insert desired short URL:</p>
+      <input id="desiredurl" type="text"><br>
+      <button id="shorten" type="submit">Get shortened URL</button><br>
+      <div>* First you should insert your URL and then push the button to get the URL shortened. You may also insert preffered short URL.</div>
+    </form>
+  </body>
+</html>
