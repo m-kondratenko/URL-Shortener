70213ec3a917c278dfcf072cb514671791241af9
m-kondratenko <madcat1202@yandex.ru>
2016-12-23 17:09:23 +0300

    The following features have been added: - verifying of inserted URL; - possibility to insert desired URL;

--- js/main.js
+++ js/main.js
@@ -3,13 +3,12 @@ function GetShortURL() {
     url:'/php/getshorturl.php',
     type:'POST',
     cache:false,
-    data: {longurl:$('#longurl').val(), shorturl:$('#desiredurl').val()},
+    data: {longurl:$('#longurl').val(), desiredurl:$('#desiredurl').val()},
     success: function(data) {
       if (data.indexOf('db_error') + 1) {
         alert('There is no connection to the DB')
       }
       else {
-        alert(data);
         $('#shorturl').val(data);
       }
     }
--- php/getshorturl.php
+++ php/getshorturl.php
@@ -1,26 +1,49 @@
 <?php
-  require_once 'DB.php';
-  //check for injections
-  if(get_magic_quotes_gpc()==1)  {
-    $longurl=stripslashes(trim($_POST["longurl"]));
-    $desiredurl=stripslashes(trim($_POST["desiredurl"]));
-    }
-  else {
-    $longurl=trim($_POST["longurl"]);
-    $desiredurl=trim($_POST["desiredurl"]);
-    }
-  $longurl=mysql_real_escape_string($longurl);
-  $desiredurl=mysql_real_escape_string($desiredurl);
+  function implementFilters($url) {
+    if(get_magic_quotes_gpc()==1)  {
+      $url=stripslashes(trim($url));
+      }
+    else {
+      $url=trim($url);
+      }
+    $url=mysql_real_escape_string($url);
+    return $url;
+  }
 
+  function verifyUrl($url) {
+    $curl=curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($curl);
+    $response=curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+    return (!empty($response)&&$response!=404);
+  }
+
+  //check for valid URL
+  if (!verifyUrl($_POST["longurl"])) {
+    die("Your URL is not valid");
+  }
+  //check for injections
+  $longurl=implementFilters($_POST["longurl"]);
+  //check for desiredurl
+  if($_POST["desiredurl"]) {
+    $shorturl="http://".$_SERVER["HTTP_HOST"]."/".implementFilters($_POST["desiredurl"]);
+  }
+  else {
+    $shorturl="http://".$_SERVER["HTTP_HOST"]."/empty";
+  }
+  //insert url pair into the DB
+  require_once 'DB.php';
   $connect=connectDB();
   if ($connect->connect_errno) {
     echo "db_error";
   }
   else {
     $lock=$connect->query("LOCK TABLES `urls` WRITE");
-    $success=$connect->query("INSERT INTO  `urls` (`id`, `longurl`, `shorturl`, `date`, `usage`) VALUES (NULL, '$longurl', '$longurl', CURRENT_TIMESTAMP, '0')");
+    $success=$connect->query("INSERT INTO  `urls` (`id`, `longurl`, `shorturl`, `date`, `usage`) VALUES (NULL, '$longurl', '$shorturl', CURRENT_TIMESTAMP, '0')");
     $unlock=$connect->query("UNLOCK TABLES");
     $connect->close();
-    echo $longurl;
+    echo $shorturl;
   }
 ?>
